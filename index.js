088cd62dd1c5d201b2577d5751b44cb796466778
Moringa School <dwangui799@gmail.com>
2022-10-04 15:35:35 +0300

    complited assighnment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hoper';
+
+const welcomeMessage = 'welcome to flatbook, $ {currentUser} !' ;
+
+const exitedwelcomeMessage = welcomeMessage . toUpperCase ();
+
+const shortGreeting = 'welcome, ${currentUser.slice (0, 1 ) } !' ;
+
+
+
+
+
